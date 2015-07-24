df23f8115fcc23c51578b4f51136cb119b93d800
PrakashSudula <prakash.msmca@gmail.com>
2015-07-24 20:10:53 +0530

    inverse matrix logic with comments
    provided the comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## This is to store the computed inverse matric, means caching the value.
+
+## This function is to store the computed inverse matrix.
+##i.e. through this function values are available in cache if it was already executed at least once.
 makeCacheMatrix <- function(x = matrix()) {
   
   i <- NULL
@@ -26,9 +26,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## This function will check for alreday cached inverse matrix,if available then return that value
 ## if not available then compute the inverse of given matrix, and then return it.
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
   
   i <- x$getminv()
   
@@ -47,5 +45,34 @@ cacheSolve <- function(x, ...) {
   
 }
 
+##below comments will tell us about how to get the inverse of given matrix.
 
+## > x <- matrix(1:4,2,2)
+## > x
+## if we execuets the x then below matrix will be output
+##       [,1] [,2]
+## [1,]    1    3
+## [2,]    2    4
+
+##pass the x to function and then store the result in m
+## > m <- makeCacheMatrix(x)
+## get the values
+## > m$get()
+##      [,1] [,2]
+## [1,]    1    3
+## [2,]    2    4
+
+## execute the cacheSolve function, it return the inverse matrix 
+## > cacheSolve(m)
+##      [,1] [,2]
+## [1,]   -2  1.5
+## [2,]    1 -0.5
+
+## execute again, this time values are returned from cache.
+## > cacheSolve(m)
+
+## Get inverse matrix from cache
+##      [,1] [,2]
+## [1,]   -2  1.5
+## [2,]    1 -0.5
 
